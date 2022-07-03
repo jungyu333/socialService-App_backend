6944af089e4eebbb6cadeea8d2f61f9b407603f1
jungyu <jungyu@naver.com>
2022-07-03 22:39:32 +0900

    User info edit implement

--- src/routes/editInfo.js
+++ src/routes/editInfo.js
@@ -3,7 +3,6 @@ const multer = require("multer");
 const path = require("path");
 const { isLoggedIn } = require("./middlewares");
 const { User } = require("../../models");
-const fs = require("fs");
 
 const router = express.Router();
 
@@ -30,21 +29,29 @@ router.post(
   }
 );
 
-router.patch("/editinfo", isLoggedIn, async (req, res, next) => {
-  try {
-    await User.update(
-      {
-        nickname: req.body.nickname,
-      },
-      {
-        where: { id: req.user.id },
-      }
-    );
-    res.status(200).json({ nickname: req.body.nickname });
-  } catch (error) {
-    console.error(error);
-    next(error);
+router.post(
+  "/editinfo",
+  isLoggedIn,
+  avatarEdit.none(),
+  async (req, res, next) => {
+    try {
+      await User.update(
+        {
+          nickname: req.body.nickname,
+          avatar: req.body.avatar,
+        },
+        {
+          where: { id: req.user.id },
+        }
+      );
+      res
+        .status(200)
+        .json({ nickname: req.body.nickname, avatar: req.body.avatar });
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
   }
-});
+);
 
 module.exports = router;
